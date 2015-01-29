a7470a6d617a42a3caccc5648b0333a1118847d3
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-01-29 15:40:04 +0000

    Gzip size

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-var config = {
-    basePath: '../frontend',
-    staticFolder: 'static/src/javascripts',
-    seeds: [
-        'bootstraps/app.js',
-        'bootstraps/commercial.js',
-        'core.js'
-    ],
-    limit: 30,
-    destination: 'tmp',
-    verbose: false
+module.exports = function (config) {
+    config = config || {
+        basePath: '../frontend',
+        staticFolder: 'static/src/javascripts',
+        seeds: [
+            'bootstraps/app.js',
+            'bootstraps/commercial.js',
+            'core.js'
+        ],
+        limit: 30,
+        destination: 'tmp/summary.html',
+        verbose: false
+    };
+
+    var Promise = require('es6-promise').Promise;
+    var files = require('./lib/files')(config);
+    var dependencies = require('./lib/dependencies')(config);
+    var git = require('./lib/git')(config);
+    var output = require('./lib/output')(config);
+    var analize = require('./lib/analize')(config);
+
+    Promise.all([
+        files.findDependencies(dependencies.iterate),
+        git.getAllMergedPullRequests(),
+        files.getFilesStat()
+    ])
+    .then(function (result) {
+        var graph  = result[0],
+            merges = result[1],
+            stat   = result[2],
+            dependentMap = dependencies.reverseGraph(graph),
+            matrix = dependencies.matrix(graph);
+
+        return analize(dependentMap, merges, graph, matrix, stat);
+    })
+    .then(function (total) {
+        output.toHTML(total);
+    })
+    .catch(function (error) {
+        console.trace(error);
+    });
 };
 
-var Promise = require('es6-promise').Promise;
-var files = require('./lib/files')(config);
-var dependencies = require('./lib/dependencies')(config);
-var git = require('./lib/git')(config);
-var output = require('./lib/output')(config);
-var analize = require('./lib/analize')(config);
-
-Promise.all([
-    files.findDependencies(dependencies.iterate),
-    git.getAllMergedPullRequests()
-])
-.then(function (result) {
-    var graph = result[0],
-        merges = result[1],
-        dependentMap = dependencies.reverseGraph(graph),
-        matrix = dependencies.matrix(graph);
-
-    return analize(dependentMap, merges, graph, matrix);
-})
-.then(function (total) {
-    output.toHTML(total);
-})
-.catch(function (error) {
-    console.trace(error);
-});
+if (require.main === module) {
+    module.exports();
+}
